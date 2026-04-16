e2c045e26d68cf517759db08d9887079d2573001
InternalDev-SBFC <internaldev@sbfc.com>
2026-04-16 12:52:57 +0530

    new file added

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client')));
+
+app.get('/api/hello', (req, res) => {
+    res.json({ message: 'CI/CD is working!' });
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'index.html'));
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
